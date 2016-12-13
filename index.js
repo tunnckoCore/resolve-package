6d05674208feab8a82a199babb1766f0e170c225
tunnckoCore <mameto_100@mail.bg>
2016-12-13 07:49:01 +0200

    docs(api): update api docs

--- index.js
+++ index.js
@@ -11,11 +11,52 @@ const fs = require('fs')
 const path = require('path')
 const get = require('get-installed-path')
 
-const readPackage = (fp) => new Promise((resolve, reject) => {
-  fs.readFile(path.resolve(fp, 'package.json'), 'utf8', (err, str) => {
-    if (err) return reject(err)
-    const json = JSON.parse(str)
-    resolve(json)
+/**
+ * > Get full absolute path of package with `name` from
+ * local node_modules or from globally installed.
+ *
+ * **Example**
+ *
+ * ```js
+ * const resolvePackage = require('resolve-package')
+ *
+ * resolvePackage('npm').then((fp) => {
+ *   console.log(fp)
+ *   // => '~/.nvm/versions/node/v7.0.0/lib/node_modules/npm/lib/npm.js'
+ * })
+ *
+ * resolvePackage('standard').then((fp) => {
+ *   console.log(fp)
+ *   // => '~/.nvm/versions/node/v7.0.0/lib/node_modules/standard/index.js'
+ * })
+ *
+ * resolvePackage('get-installed-path').then((fp) => {
+ *   console.log(fp)
+ *   // => '~/code/resolve-package/node_modules/get-installed-path/index.js'
+ * })
+ *
+ * resolvePackage('foo-quqixs-dasdasdh').catch((err) => {
+ *   console.error(err) // => Error module not found
+ * })
+ * ```
+ *
+ * @name   resolvePackage
+ * @param  {String}   `name` package name
+ * @param  {Function} `opts` optional options such as below
+ * @param  {String}   `opts.cwd` directory where is the `node_modules` folder
+ * @param  {String}   `opts.mainFile` main file for directories, default `index.js`
+ * @param  {String}   `opts.mainField` name of the package.json's "main" field, default `main`
+ * @return {Promise}
+ * @api public
+ */
+
+const resolvePackage = (name, opts) => new Promise((resolve, reject) => {
+  opts = opts && typeof opts === 'object' ? opts : {}
+  opts.local = true
+
+  get(name, opts).then(tryLoad(opts, resolve), (e) => {
+    opts.local = false
+    get(name, opts).then(tryLoad(opts, resolve), reject)
   })
 })
 
@@ -40,13 +81,11 @@ const tryLoad = (opts, resolve) => (fp) => {
   )
 }
 
-const resolvePackage = (name, opts) => new Promise((resolve, reject) => {
-  opts = opts && typeof opts === 'object' ? opts : {}
-  opts.local = true
-
-  get(name, opts).then(tryLoad(opts, resolve), (e) => {
-    opts.local = false
-    get(name, opts).then(tryLoad(opts, resolve), reject)
+const readPackage = (fp) => new Promise((resolve, reject) => {
+  fs.readFile(path.resolve(fp, 'package.json'), 'utf8', (err, str) => {
+    if (err) return reject(err)
+    const json = JSON.parse(str)
+    resolve(json)
   })
 })
 
